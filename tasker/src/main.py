4bd2203a17d17b5d8d1643fd28982de6cb337f21
DenizenB <github@podd.xyz>
2022-08-24 15:44:45 +0000

    prepare for asyncio

--- tasker/src/main.py
+++ tasker/src/main.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
 import logging
+from datetime import timedelta
+from asyncio import run, sleep
 
 from job import UrlscanJob
+
+JOB_INTERVAL = 60
+JOBS = [
+    UrlscanJob("#phishing", tags=["#phishing"]),
+    UrlscanJob("#gootloader", tags=["#gootloader"], referer="https://www.bing.com/"),
+]
+
+async def main():
+    while True:
+        for job in JOBS:
+            try:
+                job.run()
+            except:
+                logging.exception(f"Failed to run job: {job.twitter_query}")
+
+        await sleep(JOB_INTERVAL)
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
 
-    jobs = [
-        UrlscanJob("#phishing", tags=["#phishing"]),
-        UrlscanJob("#gootloader", tags=["#gootloader"], referer="https://www.bing.com/"),
-    ]
-
-    for job in jobs:
-        job.run()
+    run(main())
