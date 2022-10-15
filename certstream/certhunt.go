87a59fc1589412ebb1b39112005b3b1c3fb186d4
DenizenB <github@podd.xyz>
2022-10-15 10:52:45 +0000

    clean up rules, output json misp attributes

--- certstream/certhunt.go
+++ certstream/certhunt.go
@@ -4,18 +4,28 @@ import (
     "os"
     "time"
     "strings"
+    "encoding/json"
     "fmt"
 
     logging "github.com/op/go-logging"
     "github.com/CaliDog/certstream-go"
+    "github.com/jmoiron/jsonq"
     "github.com/paulbellamy/ratecounter"
-    "golang.org/x/exp/slices"
     "golang.org/x/net/publicsuffix"
 )
 
+type MispAttribute struct {
+    ParentEvent string `json:"parent_event"`
+    Event string `json:"event"`
+    EventTags []string `json:"event_tags"`
+    Type string `json:"type"`
+    Value string `json:"value"`
+    Comment string `json:"comment"`
+}
+
 var log = logging.MustGetLogger("certhunt")
 
-func streamCerts(outputStream chan<- DynamicMap) {
+func streamCerts(outputStream chan<- map[string]interface{}) {
     printInterval := 10 * time.Second
     var lastPrint = time.Now()
 
@@ -37,30 +47,32 @@ func streamCerts(outputStream chan<- DynamicMap) {
                 data, _ := jq.Object("data")
 
                 // Enrich message
-                if leaf_cert, ok := data["leaf_cert"]; ok {
-                    leaf_cert := leaf_cert.(map[string]interface{})
+                if leafCert, ok := data["leaf_cert"]; ok {
+                    leafCert := leafCert.(map[string]interface{})
 
-                    if all_domains, ok := leaf_cert["all_domains"]; ok {
-                        all_domains := all_domains.([]interface{})
+                    if allDomains, ok := leafCert["all_domains"]; ok {
+                        allDomains := allDomains.([]interface{})
 
-                        registered_domains := make([]string, 0)
-                        for _, domain := range all_domains {
-                            registered_domain, err := publicsuffix.EffectiveTLDPlusOne(domain.(string))
+                        registeredDomains := make([]interface{}, 0)
+                        alreadyAdded := map[string]struct{}{}
+                        for _, domain := range allDomains {
+                            registeredDomain, err := publicsuffix.EffectiveTLDPlusOne(domain.(string))
                             if err != nil {
                                 log.Debug(err)
                                 continue
                             }
 
-                            if !slices.Contains(registered_domains, registered_domain) {
-                                registered_domains = append(registered_domains, registered_domain)
+                            if _, exists := alreadyAdded[registeredDomain]; !exists {
+                                alreadyAdded[registeredDomain] = struct{}{}
+                                registeredDomains = append(registeredDomains, registeredDomain)
                             }
                         }
 
-                        leaf_cert["registered_domains"] = registered_domains
+                        leafCert["registered_domains"] = registeredDomains
                     }
                 }
 
-                outputStream<- DynamicMap(data)
+                outputStream<- data
             case err := <-errors:
                 log.Debug(err)
         }
@@ -74,7 +86,7 @@ func streamCerts(outputStream chan<- DynamicMap) {
     }
 }
 
-func matchCerts(inputStream <-chan DynamicMap) {
+func matchCerts(inputStream <-chan map[string]interface{}) {
     // Load rules
     log.Debug("Loading Sigma rules")
     ruleset, err := LoadRules("./rules")
@@ -84,23 +96,45 @@ func matchCerts(inputStream <-chan DynamicMap) {
 
     // Match certs
     for {
-        cert_data := <-inputStream
+        certData := <-inputStream
+        event := DynamicMap(certData)
 
         for _, rule := range ruleset.Rules {
-            if result, match := rule.Eval(cert_data); match {
-                fingerprint, _ := cert_data.Select("leaf_cert.fingerprint")
-                fingerprint = strings.ToLower(strings.ReplaceAll(fingerprint.(string), ":", ""))
+            if result, match := rule.Eval(event); match {
+                jq := jsonq.NewQuery(certData)
 
-                output := fmt.Sprintf("Match for \"%s\" (%s)", result.Title, fingerprint)
-                output += "\n\tDescription: " + FormatRuleDescription(rule.Rule, cert_data)
-                output += "\n\tFields:"
-                for _, field := range rule.Rule.Rule.Fields {
-                    if value, ok := cert_data.Select(field); ok {
-                        output += fmt.Sprintf("\n\t\t%s: %s", field, value)
-                    }
+                fingerprint, _ := jq.String("leaf_cert", "fingerprint")
+                fingerprint = strings.ToLower(strings.ReplaceAll(fingerprint, ":", ""))
+                log.Infof("Match for \"%s\" (%s)", result.Title, fingerprint)
+
+                registeredDomains, err := jq.ArrayOfStrings("leaf_cert", "registered_domains")
+                if err != nil {
+                    log.Error(err)
+                    continue
                 }
 
-                log.Info(output)
+                if len(registeredDomains) == 0 {
+                    // No registered domain, no attribute
+                    log.Error("Failed to resolve registered domains for certificate")
+                    continue
+                }
+
+                attribute := MispAttribute{
+                    ParentEvent: "Certstream Sigma Rules",
+                    Event: result.Title,
+                    EventTags: result.Tags,
+                    Type: "domain",
+                    Value: registeredDomains[0],
+                    Comment: "First seen: ",
+                }
+
+                jsonAttr, err := json.Marshal(attribute)
+                if err != nil {
+                    log.Error(err)
+                    continue
+                }
+
+                fmt.Println(string(jsonAttr))
             }
         }
     }
@@ -131,7 +165,7 @@ func setupLogging() {
 func main() {
     setupLogging()
 
-    certs := make(chan DynamicMap)
+    certs := make(chan map[string]interface{})
 
     workerCount := 5
     for i := 0; i < workerCount; i++ {
